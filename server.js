c6c19a097137f1a9b9a18ec6dc9f535d6a889c73
edratner-nunn <38732412+EdRatner@users.noreply.github.com>
2025-11-01 13:34:54 +0000

    Initial Commit - Map setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+
+const port = 3000
+
+app.use(express.static('./public/'));
+
+server.listen(port, () => {
+    console.log("Server Initiated.");
+})
+
+
+
+
+
+
+
+
+
+
